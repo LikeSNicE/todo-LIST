dae7bf1742283cb7497fa6d77f5ad7707d2a80b8
Nikita Lutsenko <nik,luzenko@mail.ru>
2022-06-24 13:14:40 +0600

    changed style of code

--- app/js/main.js
+++ app/js/main.js
@@ -1,43 +1,23 @@
-
-
 const input = document.querySelector('#input'),
-  btn = document.querySelector('.btn'),
-  result = document.querySelector('.result'),
-  total = document.querySelector('.total');
-  let i = 0;
-
-  let tasks = [];
-  !localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'));
-
-  let todoImemElems = [];
-
-  function Task(description){
-    this.description = description;
-  }
-
-  // update localstorage 
-
-  const updateLocal = () => {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  };
+btn = document.querySelector('.btn'),
+result = document.querySelector('.result'),
+total = document.querySelector('.total');
+let i = 0;
 
 // animation
 function animation({
   elem = '',
   animationClass = 'animated',
   nameOfAnimation = 'fadeIn'
-} = {}){
-  elem.classList.add(animationClass,nameOfAnimation);
+} = {}) {
+  elem.classList.add(animationClass, nameOfAnimation);
 }
 
 
 // add event 
 btn.addEventListener('click', () => {
-  tasks.push(new Task(input.value));
-  updateLocal();
 
-  console.log(tasks);
-  if (input.value === "") {
+  if (input.value === ""){
     return;
   }
 
@@ -46,43 +26,37 @@ btn.addEventListener('click', () => {
 });
 
 
-const completeTask = index => {
-  if(tasks[index].description){
-
-  }
-};
-
 // create and deleme elem todo
 function createDeleteElements(value) {
+
   i++;
-
   const li = document.createElement('li');
-  const btn = document.createElement('button');
+  const btnDelete = document.createElement('button');
 
-
-  li.style.cursor = "pointer";
   animation({
     elem: li,
   });
 
-  li.classList.add('li');
-  li.textContent = value;
+   function addSomeStylesElem(elem, className, valueName) {
+     elem.style.cursor = "pointer";
+     elem.classList.add(className);
+     elem.textContent = valueName;
+   }
 
-  btn.classList.add('btn');
-  btn.textContent = 'Убрать';
-  li.appendChild(btn);
+  addSomeStylesElem(li,'li',value);
+  addSomeStylesElem(btnDelete,'btn','Убрать');
 
+  li.appendChild(btnDelete);
 
   // remove todo
-  btn.addEventListener('click', (e) => {
+  btnDelete.addEventListener('click', (e) => {
     animation({
       elem: li
     });
     setTimeout(() => {
-       i--;
-       total.textContent = i;
-       result.removeChild(li);
-       updateLocal();
+      i--;
+      total.textContent = i;
+      result.removeChild(li);
     }, 350);
   });
 
@@ -91,7 +65,18 @@ function createDeleteElements(value) {
     li.classList.toggle('li-active');
   });
 
-  animation({elem: total,});
+  animation({
+    elem: total,
+  });
   total.textContent = i;
   result.appendChild(li);
+
+  miniAchievements(20);
+
+  function miniAchievements(i) {
+    if (document.querySelectorAll('.result li').length == i) {
+       alert('Вы как Дамир, идите отдохните');
+    }
+  }
 }
+
